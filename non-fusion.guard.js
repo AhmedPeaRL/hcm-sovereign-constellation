1400fe6d9f6090c71763bf826165216d8f579c75
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-02-01 19:12:50 +0200

    Create non-fusion.guard.js

---
+++ non-fusion.guard.js
@@ -0,0 +1,16 @@
+/*
+  NON-FUSION GUARD
+
+  If you are about to:
+  - merge repositories
+  - unify codebases
+  - centralize execution
+  - create a "main app"
+
+  Stop.
+
+  Fusion destroys emergence.
+  Coherence does not require unity.
+*/
+
+export const FUSION = false;
